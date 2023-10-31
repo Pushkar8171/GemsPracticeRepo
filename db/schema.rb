dd1d2bcba7eb7edf8cb58ccf9d5fd0798df5caf5
Pushkar8171 <pushkarsinghrathore81@gmail.com>
2023-10-31 12:16:08 +0530

    update schmea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_27_070154) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_26_080438) do
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -27,7 +27,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_27_070154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_type"
-    t.string "password"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
